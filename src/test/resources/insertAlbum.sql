e19840e62a28007b254dc55d4c1839e52d64a3aa
DoganHaluk <halukdogan78@gmail.com>
2021-12-24 11:44:27 +0100

    service integration test added

--- src/test/resources/insertAlbum.sql
+++ src/test/resources/insertAlbum.sql
@@ -1,4 +1,2 @@
 insert into albums(artiestId, naam)
 values ((select id from artiesten where naam = 'test'), 'test');
-insert into tracks(albumId, naam, tijd)
-values ((select id from albums where naam = 'test'), 'test', CURRENT_TIME);
